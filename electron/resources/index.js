ab0ba455590a9376686f12bde14f5aa34a172c6c
scanhand <scanhand@gmail.com>
2018-01-17 19:39:43 +0900

    added using jquery import

--- electron/resources/index.js
+++ electron/resources/index.js
@@ -1,5 +1,6 @@
-var boots = require("bootstrap");
-
-console.log(boots);
-
-document.write("<h1>Index.js First Write!</h1>");
+import $ from "jquery";
+// var boots = require("bootstrap");
+// console.log(boots);
+// import 'jquery';
+// import 'bootstrap';
+$("#JQTest").html("<h1>Index.js First Write!</h1>");
