dff5385a0d72f843607dcada0d7add4bb3301ba4
scanhand <scanhand@gmail.com>
2018-01-29 20:31:51 +0900

    modify ffi loading relative path added NodeDCMTK solution file

--- electron/resources/app/JS/index.js
+++ electron/resources/app/JS/index.js
@@ -3,4 +3,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import menu from './menu.js';
 
-// var ffi = require('ffi');
+
+var oldPath = process.env.PATH;
+///현재 path 기준은 electron.exe
+var dllPath = '..\\Release';
+process.env['PATH'] = `${process.env.PATH};${dllPath}`;
+// binding to a nodeDCMTK functions...
+var nodeDCMTK = ffi.Library('NodeDCMTK.dll', {
+'test_dcmtk': [ 'int', [ 'int', 'int' ]]
+});
+process.env['PATH'] = oldPath;
+
+//call test_dcmtk
+var result = nodeDCMTK.test_dcmtk(3,4);
+console.log("3+4=" + result);
--- electron/resources/app/build/bundle.js
+++ electron/resources/app/build/bundle.js
@@ -10347,8 +10347,20 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
-// var ffi = require('ffi');
 
+var oldPath = process.env.PATH;
+///현재 path 기준은 electron.exe
+var dllPath = '..\\Release';
+process.env['PATH'] = `${process.env.PATH};${dllPath}`;
+// binding to a nodeDCMTK functions...
+var nodeDCMTK = ffi.Library('NodeDCMTK.dll', {
+'test_dcmtk': [ 'int', [ 'int', 'int' ]]
+});
+process.env['PATH'] = oldPath;
+
+//call test_dcmtk
+var result = nodeDCMTK.test_dcmtk(3,4);
+console.log("3+4=" + result);
 
 /***/ }),
 /* 3 */
