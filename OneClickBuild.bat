190f9018c9bc52345014ad9cab83bff7d51c7e3c
scanhand <scanhand@hanmail.net>
2018-04-23 19:49:01 +0900

    build error in OneClickBuild.bat bug fixed

--- OneClickBuild.bat
+++ OneClickBuild.bat
@@ -1,8 +1,11 @@
 @echo off
-@echo Start eDICOM Viewer Building...
+@echo ##### Start eDICOM Viewer Building... ##### 
+@echo 
 cd ./electron/resources/app/JS
-@echo installing module by npm
+@echo ### 1.Installing module by npm ##### 
 call npm install
-@echo packaging by webpack
+@echo ### 2.Start electron-rebuild by npm ##### 
+call ./node_modules/.bin/electron-rebuild
+@echo ### 3.Packaging by webpack ##### 
 call webpack
 pause
--- electron/resources/app/main.js
+++ electron/resources/app/main.js
@@ -10,12 +10,17 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({width: 1024, height: 600})
+
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true,
   }))
+
+  // Debugging
+  // log.debug(process.versions);
+  // win.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
