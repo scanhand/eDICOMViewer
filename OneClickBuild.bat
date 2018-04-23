36874b320029766b8487eb774aadfb11ac2d76b1
scanhand <scanhand@hanmail.net>
2018-04-23 20:28:07 +0900

    added electron verion on electron-rebuild

--- OneClickBuild.bat
+++ OneClickBuild.bat
@@ -1,11 +1,14 @@
 @echo off
-@echo ##### Start eDICOM Viewer Building... ##### 
 @echo. 
+@echo ##### Start eDICOM Viewer Building... #####
 cd ./electron/resources/app/JS
-@echo ### 1.Installing module by npm ##### 
+@echo. 
+@echo ## 1.Installing module by npm ##
 call npm install
-@echo ### 2.Start electron-rebuild by npm ##### 
-call ./node_modules/.bin/electron-rebuild
-@echo ### 3.Packaging by webpack ##### 
+@echo. 
+@echo ## 2.Start electron-rebuild by npm ##
+call ./node_modules/.bin/electron-rebuild -v1.7.10
+@echo. 
+@echo ## 3.Packaging by webpack ##
 call webpack
 pause
